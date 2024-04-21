53776ff80f3d6a6deee707691db433fddfe2c43c
MalteHerrmann <malte@evmos.org>
2024-04-21 21:26:56 +0200

    commit first working prototype

---
+++ main.py
@@ -0,0 +1,47 @@
+"""
+This tool can be used to assign ID3 metadata to audio files,
+which is a common operation when managing my Traktor DJ tracks library.
+"""
+
+import os
+import sys
+from pathlib import Path
+from typing import List
+
+from metadata import adjust_metadata
+
+
+def get_paths(path: str) -> List[Path]:
+    """
+    Returns a list of paths from the given arguments.
+    """
+
+    if not os.path.exists(path):
+        print(f"Given path does not exist: {path}")
+        return []
+
+    paths = []
+    if os.path.isfile(path):
+        paths.append(Path(path))
+    elif os.path.isdir(path):
+        for entry in os.scandir(path):
+            if entry.is_file():
+                paths.append(Path(entry.path))
+    else:
+        print(f"Given path is neither file nor directory: {path}")
+
+    return paths
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print(
+            "You need to specify a specific file " +
+            "or a directory containing mp3s!"
+        )
+        sys.exit(1)
+
+    files = get_paths(sys.argv[1])
+    for file in files:
+        print("processing file: ", file)
+        adjust_metadata(file, comments="", genre="House")
---
+++ metadata.py
@@ -0,0 +1,68 @@
+import os
+from pathlib import Path
+
+import eyed3
+
+
+def set_comments(mp3_file, comments: str):
+    """
+    Helper function to set the comments field on a given MP3 file.
+    """
+
+    mp3_file.tag.comments.set(comments)
+
+
+def get_comments(mp3_file) -> str | None:
+    """
+    Helper function to return the comments field from the given MP3 file.
+    """
+
+    comments = mp3_file.tag.comments.get("")
+    return None if comments is None else comments.text
+
+
+def adjust_metadata(path: Path, comments: str, genre: str):
+    """
+    Adjusts the metadata of the given MP3 file,
+    in case it is not already defined.
+    """
+
+    mp3_file = eyed3.load(path)
+    if mp3_file is None:
+        raise ValueError("Could not load the MP3 file.")
+
+    filename = os.path.splitext(path.name)[0]
+    split_name = filename.split(" - ")
+    if len(split_name) != 2:
+        raise ValueError(f"Unexpected filename syntax: {path.name}")
+
+    artist, title = split_name
+    file_adjusted = False
+
+    if mp3_file.tag.artist is None:
+        mp3_file.tag.artist = artist
+        file_adjusted = True
+    else:
+        print(f"Artist already filled: {mp3_file.tag.artist}")
+
+    if mp3_file.tag.title is None:
+        mp3_file.tag.title = title
+        file_adjusted = True
+    else:
+        print(f"Title already filled: {mp3_file.tag.title}")
+
+    available_comments = get_comments(mp3_file)
+    if available_comments is None:
+        set_comments(mp3_file, comments)
+        file_adjusted = True
+    else:
+        print(f"Comments already filled: {available_comments}")
+
+    if mp3_file.tag.genre is None:
+        mp3_file.tag.genre = genre
+        file_adjusted = True
+    else:
+        print(f"Genre already filled: {mp3_file.tag.genre}")
+
+    if file_adjusted:
+        mp3_file.tag.save()
